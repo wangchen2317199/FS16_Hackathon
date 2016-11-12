e66d8096f327fa438b00c0b17afd4cd867195b5c
liuyunl777 <yunlongliu@yunlongs-mbp.kc.umkc.edu>
2016-11-12 16:53:41 -0600

    high chart

--- src/app/user_view/overview.js
+++ src/app/user_view/overview.js
@@ -25,42 +25,100 @@ $(document).ready(
                   
                    //if success show the graph
                   
-                  
-                 
-//var uri = "http://finance.google.com/finance/info?client=ig&q=NSE:TAAAX";
-//
-// $.ajax(
-//            {
-//                url : uri,
-//                method: 'GET',
-//                success: function(result) {
-//                
-//                var str = result ;
-//                var res = str.substring(4,res.length - 1);
-//
-//                console.log(res);
-//
-//                },
-//                error: function(error) {
-//                    alert('Error!  Cannot get information!');
-//                   // localStorage.setItem('logged_in', false);
-//                  //  window.location.href = '../../index.html';
-//                }
-//
-//}
-//);
 
-           
+           // GOOGLE CHART
 //--------------------------------------------------------------------------------------------------------------------
                     
-                  var data = new google.visualization.DataTable();
-                    data.addColumn('number', 'X');
-                    data.addColumn('number', 'saving with desired interest');
+//                  var data = new google.visualization.DataTable();
+//                    data.addColumn('number', 'X');
+//                    data.addColumn('number', 'saving with desired interest');
+//
+//                  
+//                 var d = new Date() ;
+//                  var d  = Date.parse(d);
+//                var D = Date.parse(DOB);
+//                  var age = d - D;
+//                  var minutes = 1000 * 60;
+//                  var hours = minutes * 60;
+//                  var days = hours * 24;
+//                  var years = days * 365;
+//
+//                  var currentAge = Math.round(age / years);
+//                  
+//                      
+//               //  var idealInterest = [];
+//                   var income = parseFloat(income);
+//                   var savings = parseFloat(savings);
+//                   var retire_age = parseFloat(retire_age);
+//                   var desired_interest = parseFloat(desired_interest);
+//               
+//                   var totalSavings = 0;
+//                   var inflation = .04;
+//                   var death = 80;
+//                   var expense = income - savings;
+//                  
+//                var actualRate =(desired_interest/ 100.0)  - inflation ;
+//  
+//                  
+//                  for ( i = currentAge; i <= retire_age; i++ ) {
+//                    totalSavings = (totalSavings + savings) * (1 + actualRate);
+//                    totalSavings = Math.round(totalSavings);
+//                 //   idealInterest[i] = totalSavings;
+//                    console.log("year = " + i + " amount = " + totalSavings);
+//                  console.log(savings);
+//                 // data.addRow();
+//                 // data.addRow([i,totalSavings]);
+//                  
+//                }
+//
+//                for ( i = retire_age + 1; i <= death; i++ ) {
+//                    totalSavings = (totalSavings - expense) * (1 + actualRate);
+//                    totalSavings = Math.round(totalSavings);
+//                //  idealInterest[i] = totalSavings;
+//                    console.log("second loop year = " + i + " amount = " + totalSavings);
+//                  
+//                 // data.addRow();
+//                 // data.addRow([i,totalSavings]);
+//                  
+//                }
+//              
+//
+//  
+//
+//      var options = {
+//        hAxis: {
+//          title: 'Age'
+//        },
+//        vAxis: {
+//          title: 'Total Saving'
+//        },
+//        series: {
+//          1: {curveType: 'function'}
+//        },
+//        
+//        backgroundColor :{
+//        fill:'transparent'
+//        
+//      }
+//      };
+//
+//      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+//      chart.draw(data, {width: 500, height: 500});
+//      chart.draw(data, options);
+//                  
+              
+//--------------------------------------------------------------------------------------------------------------------
+
+//         high chart         
+//--------------------------------------------------------------------------------------------------------------------
 
                   
-                 var d = new Date() ;
+                  
+                  
+               
+                  var d = new Date() ;
                   var d  = Date.parse(d);
-                var D = Date.parse(DOB);
+                  var D = Date.parse(DOB);
                   var age = d - D;
                   var minutes = 1000 * 60;
                   var hours = minutes * 60;
@@ -69,70 +127,117 @@ $(document).ready(
 
                   var currentAge = Math.round(age / years);
                   
-                      
-               //  var idealInterest = [];
+                   
                    var income = parseFloat(income);
                    var savings = parseFloat(savings);
                    var retire_age = parseFloat(retire_age);
                    var desired_interest = parseFloat(desired_interest);
-               //    var currentAge = parseFloat('30');
+               
                    var totalSavings = 0;
                    var inflation = .04;
                    var death = 80;
                    var expense = income - savings;
                   
-                var actualRate =(desired_interest/ 100.0)  - inflation ;
-  
+                   var actualRate =(desired_interest/ 100.0)  - inflation ;  
                   
-                  for ( i = currentAge; i <= retire_age; i++ ) {
+                  
+                    var agearray = [];
+                  for (i = currentAge; i <= 80; i++ ){
+                    
+                    agearray.push(i);
+                    
+                  }
+                  
+                  
+                   var idealinterest = [];
+                   for ( i = currentAge; i <= retire_age; i++ ) {
                     totalSavings = (totalSavings + savings) * (1 + actualRate);
                     totalSavings = Math.round(totalSavings);
-                 //   idealInterest[i] = totalSavings;
+            
                     console.log("year = " + i + " amount = " + totalSavings);
-                  console.log(savings);
-                 // data.addRow();
-                  data.addRow([i,totalSavings]);
-                  
+                    console.log(savings);
+                     
+                      idealinterest.push(totalSavings);
+            
                 }
 
                 for ( i = retire_age + 1; i <= death; i++ ) {
                     totalSavings = (totalSavings - expense) * (1 + actualRate);
                     totalSavings = Math.round(totalSavings);
-                //    idealInterest[i] = totalSavings;
+                //  idealInterest[i] = totalSavings;
                     console.log("second loop year = " + i + " amount = " + totalSavings);
-                  
-                 // data.addRow();
-                  data.addRow([i,totalSavings]);
-                  
+            
+                  idealinterest.push(totalSavings);
                 }
-              
-
-  
-
-      var options = {
-        hAxis: {
-          title: 'Age'
-        },
-        vAxis: {
-          title: 'Total Saving'
-        },
-        series: {
-          1: {curveType: 'function'}
-        },
-        
-        backgroundColor :{
-        fill:'transparent'
-        
+                   console.log(idealinterest);
+                 console.log(agearray);
+                  
+                  
+           Highcharts.chart('container', {
+             chart: {
+      backgroundColor: null,
+      style: {
+         fontFamily: 'Signika, serif'
       }
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 500, height: 500});
-      chart.draw(data, options);
+   },
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+          title: {
+                text: 'Age'
+            },
+            categories: agearray
+//          ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+//                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Total Saving'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+          plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'saving with idealinterest',
+            data: idealinterest},
+           {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }
+        ]
+    });       
+                  
+                  
                   
                   
 //--------------------------------------------------------------------------------------------------------------------
-                },
+                     },
                 error: function(error) {
                     alert('Error!  Cannot get information!');
                     localStorage.setItem('logged_in', false);
